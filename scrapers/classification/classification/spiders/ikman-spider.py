0c90fc9e29d4db8ee0174247741ff9b096c0f809
dilushad <dilushad@zone24x7.lk>
2020-10-31 03:20:49 +0530

    scrape from pages

--- scrapers/classification/classification/spiders/ikman-spider.py
+++ scrapers/classification/classification/spiders/ikman-spider.py
@@ -3,9 +3,26 @@ import json
 
 class IkmanSpider(scrapy.Spider):
     name = "ikman"
-    start_urls = ["https://ikman.lk/en/ad/mercedes-benz-c200-kompressor-2007-for-sale-colombo-1"]
+    start_urls = [
+        "https://ikman.lk/en/ads/sri-lanka/cars",
+        "https://ikman.lk/en/ads/sri-lanka/vans-buses-lorries"
+        ]
 
     def parse(self, response):
+        for url in self.start_urls:
+            for page_no in range(5):
+                final_url = url + "?page=" + str(page_no)
+                yield scrapy.Request(final_url, dont_filter=True, callback=self.scrapeList)
+
+    def scrapeList(self, response):
+        base_url = "https://ikman.lk"
+
+        link_list = response.xpath("//*[contains(@class, 'normal--2QYVk gtm-normal-ad')]//a/@href")
+
+        for link in link_list:
+            yield scrapy.Request(base_url + link.get(), dont_filter=True, callback=self.scrapeSinglePage)
+
+    def scrapeSinglePage(self, response):
 
         addvertiesment = response.xpath(".//div[contains(@class, 'section--PpGYD')]")
         details = addvertiesment.xpath(".//*[contains(@class, 'two-columns--19Hyo')]")
